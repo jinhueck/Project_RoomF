5210d6599c37d4620443553727511f8d870a6b24
jinhueck <qorgh9184@gmail.com>
2018-11-20 11:51:20 +0900

    project 파일 추가

---
+++ Assets/Move_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Script : MonoBehaviour {
+    public Vector3 pos;
+    public Vector3 nowPos;
+	// Use this for initialization
+	void Start () {
+        nowPos = transform.position - pos;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vec = this.transform.position - pos;
+        Debug.Log("vec : " + vec);
+        //transform.GetChild(0).transform.position = transform.position + vec.normalized * 0.2f;
+        transform.GetChild(0).transform.position = transform.position + new Vector3(0,-0.1f,0) + vec.normalized * 0.2f;
+    }
+}
---
+++ Assets/Real_Move_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Real_Move_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 vec = (transform.forward * h * -1 + transform.right * v ) * Time.deltaTime;
+        transform.Translate(vec);
+	}
+}
