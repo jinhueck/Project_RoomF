cf7305c75011dc65e79343da9c2910a622781c1d
bockdung <slzxc373@gmail.com>
2018-11-28 16:41:34 +0900

    뿌아앙

---
+++ Assets/LHJ/NaviMons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NaviMons : MonoBehaviour {
+
+    NavMeshAgent nav;
+    Collider target;
+
+    void Start ()
+    {
+        nav = transform.parent.GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+    {
+        if(target!=null)
+        nav.destination = target.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        target = other;
+    }
+}
