b70e2680dbf15d4626a0f6372dbbc66fd753b330
bockdung <slzxc373@gmail.com>
2018-12-12 17:29:54 +0900

    튜토리얼 작업중

---
+++ Assets/follow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+    public Transform Player;
+    Vector3 Pos;
+
+	// Use this for initialization
+	void Start () {
+        Pos = transform.position - Player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Player.transform.position + Pos;
+	}
+}
